9ee1e085985bfbdcddd9f8dcfa637682fbb61d02
Andrew Venson <vensoneandrew@gmail.com>
2020-03-28 00:40:14 -0500

    zoominout

--- blueprints/dash/static/dash/js/global-widget.js
+++ blueprints/dash/static/dash/js/global-widget.js
@@ -5,18 +5,8 @@ $('.search').click(function(){
     }else{
         $('.titles').css('display', 'none');
         $('.search-inp').css('display', 'block');
-
     }
-})
+});
 
-$('#svg2 path').mouseenter(function(){
-    $('.country-info').css('display', 'block');
-    $('.country-title').text($(this).attr('data-name'));
-    $('.country-cases').text(this.id)
 
-})
-
-$('#svg2 path').mouseout(function(){
-    $('.country-info').css('display', 'none');
-})
 
