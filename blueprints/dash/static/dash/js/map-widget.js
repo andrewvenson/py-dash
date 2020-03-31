d9c9f7abe089bf0c4436c0c52533b98c67eb00ce
Andrew Venson <vensoneandrew@gmail.com>
2020-03-31 18:40:56 -0500

    map data layer

--- blueprints/dash/static/dash/js/map-widget.js
+++ blueprints/dash/static/dash/js/map-widget.js
@@ -42,10 +42,18 @@ $('#reset').click(function(){
 
  function initMap() {
  var map = new google.maps.Map(document.getElementById('map'), {
-    center: new google.maps.LatLng(0,0),
-    zoom: 1,
+    center: new google.maps.LatLng(39.0119,-98.4842),
+    zoom: 4,
     streetViewControl: false,
     mapTypeControl: false,
 
   });
+
+  map.data.loadGeoJson('dash/json/gz_2010_us_040_00_500k.json');
+
+    map.data.setStyle({
+        fillColor: 'green',
+        strokeWeight: 1
+      });
+
 }
