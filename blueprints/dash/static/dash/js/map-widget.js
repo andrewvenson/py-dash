5f72efab3bc83b6f8901b46cb5dc1be7d22c0ef8
Andrew Venson <vensoneandrew@gmail.com>
2020-03-31 19:13:27 -0500

    mappy

--- blueprints/dash/static/dash/js/map-widget.js
+++ blueprints/dash/static/dash/js/map-widget.js
@@ -41,19 +41,30 @@ $('#reset').click(function(){
 
 
  function initMap() {
- var map = new google.maps.Map(document.getElementById('map'), {
-    center: new google.maps.LatLng(39.0119,-98.4842),
-    zoom: 4,
-    streetViewControl: false,
-    mapTypeControl: false,
+    var map = new google.maps.Map(document.getElementById('map'), {
+        center: new google.maps.LatLng(39.0119,-98.4842),
+        zoom: 4,
+        streetViewControl: false,
+        mapTypeControl: false,
 
-  });
+    });
 
-  map.data.loadGeoJson('dash/json/gz_2010_us_040_00_500k.json');
+    map.data.loadGeoJson('dash/json/gz_2010_us_040_00_500k.json');
 
     map.data.setStyle({
         fillColor: 'green',
         strokeWeight: 1
-      });
+        });
 
-}
+    map.data.addListener('mouseover', function(event) {
+        map.data.overrideStyle(event.feature, {fillColor: 'red'});
+        $('.country-info').css('display', 'block');
+        $('.country-title').text(event.feature.getProperty("NAME"));
+        });
+    
+    map.data.addListener('mouseout', function(event) {
+        map.data.overrideStyle(event.feature, {fillColor: 'green'});
+        $('.country-info').css('display', 'none');
+        });
+
+}   
