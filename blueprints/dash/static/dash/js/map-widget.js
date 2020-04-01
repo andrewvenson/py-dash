6bb0ea25ba3f6d1e6159bf5070cde497b1986ecf
Andrew Venson <vensoneandrew@gmail.com>
2020-04-01 03:52:18 -0500

    location services

--- blueprints/dash/static/dash/js/map-widget.js
+++ blueprints/dash/static/dash/js/map-widget.js
@@ -5,6 +5,9 @@ $(".map-tab").click(function(){
    initMap();
 });
 
+var map;
+var infoWindow;
+
 function initMap() {
     if($("#world-tab").css('background-color') == 'rgb(211, 211, 211)'){
         var map = new google.maps.Map(document.getElementById('map'), {
@@ -187,21 +190,58 @@ function initMap() {
                     strokeWeight:1
                 });
         })   
-    }
+    }else if($('#loc-tab').css('background-color') == 'rgb(211, 211, 211)'){
+        
+        var map = new google.maps.Map(document.getElementById('map'), {
+            center: {lat: -34.397, lng: 150.644},
+            zoom: 7,
+            streetViewControl: false,
+            mapTypeControl: false,
+        });
+
+        infoWindow = new google.maps.InfoWindow;
+
+
+        map.data.loadGeoJson('dash/json/gz_2010_us_040_00_500k.json');
+
+        map.data.setStyle(function(feature){
+            return /** @type {!google.maps.Data.StyleOptions} */({
+                fillColor: "darkblue",
+                strokeColor: 'darkblue',
+                strokeWeight:1
+            });
+        });
+        console.log(navigator)
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(function(position) {
+            var pos = {
+                lat: position.coords.latitude,
+                lng: position.coords.longitude
+            };
+        
+            // infoWindow.setPosition(pos);
+            // infoWindow.setContent('Location found.');
+            // infoWindow.open(map);
+            map.setCenter(pos);
+            }, function() {
+            handleLocationError(true, infoWindow, map.getCenter());
+            });
+        } else {
+            // Browser doesn't support Geolocation
+            handleLocationError(false, infoWindow, map.getCenter());
+        }
+        
+        function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+        infoWindow.setPosition(pos);
+        infoWindow.setContent(browserHasGeolocation ?
+                                'Error: The Geolocation service failed.' :
+                                'Error: Your browser doesn\'t support geolocation.');
+        infoWindow.open(map);
+        }
+    
+    
+    }   
 }
 
 
-// if($('#us-tab').css('background-color') == 'rgb(211, 211, 211)'){
-//     function initMap(){
-//         var map = new google.maps.Map(document.getElementById('map'), {
-//             center: new google.maps.LatLng(31.8686,0),
-//             zoom: 3,
-//             streetViewControl: false,
-//             mapTypeControl: false,
-//         });
-        
-//         // Load geojson file to display data layer over map
-//         
-//     }
-// }
-// }
+
