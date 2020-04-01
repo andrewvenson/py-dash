990fd46c3adb19e676b89425887608e8a7893136
Andrew Venson <vensoneandrew@gmail.com>
2020-03-31 21:15:22 -0500

    get covid json data with jquery]

--- blueprints/dash/static/dash/js/map-widget.js
+++ blueprints/dash/static/dash/js/map-widget.js
@@ -1,28 +1,69 @@
- function initMap() {
+function initMap() {
     var map = new google.maps.Map(document.getElementById('map'), {
-        center: new google.maps.LatLng(39.0119,-98.4842),
-        zoom: 4,
+        center: new google.maps.LatLng(0,0),
+        zoom: 3,
         streetViewControl: false,
         mapTypeControl: false,
+    });
+    // Load geojson file to display data layer over map
+    map.data.loadGeoJson('dash/json/countries.geo.json');
+    
+    // Api Url
+    getUrlSum = "https://api.covid19api.com/summary"
 
+    // store country and totalconfirmed in dict
+    var json_countries = {}
+
+    // request json data from covid summary api url
+    $.getJSON(getUrlSum, function(data){
+        for(x in data['Countries']){
+            for(y in data['Countries'][x]){
+                json_countries[data['Countries'][x]['Country']] = data['Countries'][x]['TotalConfirmed'];
+            }
+        }
+    }).done(function(){
+
+        for(x in json_countries){
+            console.log(x, json_countries[x])
+        }
+
+        // Set style of map
+        map.data.setStyle(function(feature){
+            if (feature.getProperty('name') != "Antarctica") {
+                return /** @type {!google.maps.Data.StyleOptions} */({
+                fillColor: 'blue',
+                strokeColor: 'darkblue',
+                strokeWeight:1
+            });
+            }
+        });
     });
 
-    map.data.loadGeoJson('dash/json/gz_2010_us_040_00_500k.json');
+    
+    
 
-    map.data.setStyle({
-        fillColor: 'green',
-        strokeWeight: 1
+    map.data.setStyle(function(feature){
+        
+        if (feature.getProperty('name') != "Antarctica") {
+            return /** @type {!google.maps.Data.StyleOptions} */({
+            fillColor: 'blue',
+            strokeColor: 'darkblue',
+            strokeWeight:1
         });
+        }
+    });
 
     map.data.addListener('mouseover', function(event) {
-        map.data.overrideStyle(event.feature, {fillColor: "#005EAA"});
-        $('.country-info').css('display', 'block');
-        $('.country-title').text(event.feature.getProperty("NAME"));
-        });
+        if(event.feature.getProperty("name") != "Antarctica"){
+            map.data.overrideStyle(event.feature, {fillColor: "darkblue",strokeColor: 'red',});
+            $('.country-info').css('display', 'block');
+            $('.country-title').text(event.feature.getProperty("name"));
+        }   
+    });
     
     map.data.addListener('mouseout', function(event) {
-        map.data.overrideStyle(event.feature, {fillColor: 'green'});
+            map.data.overrideStyle(event.feature, {fillColor: 'blue',strokeColor: 'darkblue'});
         $('.country-info').css('display', 'none');
-        });
+    });
 
 }   
