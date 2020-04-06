a091aacbeb79ab8e9e5ba8c7bb976bd2b523393d
Ubuntu <ubuntu@ip-172-31-41-30.us-east-2.compute.internal>
2020-04-06 03:03:39 +0000

    public

--- run.py
+++ run.py
@@ -3,5 +3,5 @@ from blueprints import app
 
 # Allows you to run our app from this file with python directly
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0' debug=True)
 
