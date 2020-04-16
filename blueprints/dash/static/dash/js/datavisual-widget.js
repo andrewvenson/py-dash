10f920c0f6998bfd1a13b717c2b105a084a5c117
Andrew Venson <vensoneandrew@gmail.com>
2020-04-15 21:18:20 -0500

    will lee's graph changes

--- blueprints/dash/static/dash/js/datavisual-widget.js
+++ blueprints/dash/static/dash/js/datavisual-widget.js
@@ -1,41 +1,43 @@
-var confirmed_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv';
-var deaths_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv';
-var recovered_url = 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv';
+var confirmed_url =
+  "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
+var deaths_url =
+  "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv";
+var recovered_url =
+  "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_recovered_global.csv";
 
-window.addEventListener('load', setup2);
+window.addEventListener("load", setup2);
 
 async function setup2() {
-  const ctx2 = document.getElementById('active-bar').getContext('2d');
+  const ctx2 = document.getElementById("active-bar").getContext("2d");
   const World = await getData2();
   const myChart2 = new Chart(ctx2, {
-    type: 'bar',
+    type: "bar",
     data: {
       labels: World.dates,
       datasets: [
         {
-          label: 'Active Cases',
+          label: "Active Cases",
           data: World.cases_active,
-          borderColor: 'rgba(255, 99, 132, 1)',
-          backgroundColor: 'rgba(255, 99, 132, 0.5)',
-          borderWidth: 1
-        }
-      ]
+          borderColor: "rgba(255, 99, 132, 1)",
+          backgroundColor: "rgba(255, 99, 132, 0.5)",
+          borderWidth: 2,
+        },
+      ],
     },
     options: {
       title: {
         display: true,
-        text: 'Active Cases (World)'
+        text: "Active Cases (World)",
       },
       responsive: true,
       maintainAspectRatio: false,
-    }
+    },
   });
 }
 
 //get data from csv
 async function getData2() {
-  console.log('getdata');
-
+  //console.log('getdata');
   const data_all = await fetchURLs();
   var data_confirmed = data_all[0];
   var data_recovered = data_all[1];
@@ -43,48 +45,52 @@ async function getData2() {
 
   const dates = [];
   var cases_active = [];
-  const first_row = data_confirmed.split('\n')[0].split(',');
+  const first_row = data_confirmed.split("\n")[0].split(",");
 
   for (var i = 4; i < first_row.length; ++i) {
     var date = first_row[i].substring(0, 4);
-    if (date[date.length - 1] == '/') { date = date.substring(0, 3); }
+    if (date[date.length - 1] == "/") {
+      date = date.substring(0, 3);
+    }
     dates.push(date);
   }
 
-  const second_row = data_confirmed.split('\n').slice(1)[0].split(',');
+  const second_row = data_confirmed.split("\n").slice(1)[0].split(",");
 
   for (var i = 4; i < second_row.length; ++i) {
     cases_active.push(parseInt(second_row[i]));
   }
 
-  const rows_after_two = data_confirmed.split('\n').slice(2);
-  const rat_death = data_deaths.split('\n').slice(2);
-  const rat_recovered = data_recovered.split('\n').slice(2);
+  const rows_after_two = data_confirmed.split("\n").slice(2);
+  const rat_death = data_deaths.split("\n").slice(2);
+  const rat_recovered = data_recovered.split("\n").slice(2);
 
   for (var i = 0; i < rows_after_two.length; ++i) {
-    const cols = rows_after_two[i].split(',');
-    const cols2 = rat_death[i].split(',');
-    //FIX LATER
-    //const cols3 = rat_recovered[i].split(',');
-
+    const cols = rows_after_two[i].split(",");
+    const cols2 = rat_death[i].split(",");
     for (var j = 4; j < cols.length; ++j) {
-      let push_val = parseInt(cols[j]) - parseInt(cols2[j]);// - parseInt(cols2[j]);
+      let push_val = parseInt(cols[j]) - parseInt(cols2[j]);
       cases_active[j] += push_val;
     }
+  }
 
+  for (var i = 0; i < rat_recovered.length; ++i) {
+    const cols3 = rat_recovered[i].split(",");
+    for (var j = 4; j < cols3.length; ++j) {
+      cases_active[j] -= parseInt(cols3[j]);
+    }
   }
   return { dates, cases_active };
 }
 
 async function fetchURLs() {
-  console.log('fetchURLs');
-
+  //console.log('fetchURLs');
   try {
     // Promise.all() lets us coalesce multiple promises into a single super-promise
     var data = await Promise.all([
-      fetch(confirmed_url).then((response) => response.text()),// parse each response as text
+      fetch(confirmed_url).then((response) => response.text()), // parse each response as text
       fetch(recovered_url).then((response) => response.text()),
-      fetch(deaths_url).then((response) => response.text())
+      fetch(deaths_url).then((response) => response.text()),
     ]);
 
     //FOR DEBUG
@@ -98,7 +104,6 @@ async function fetchURLs() {
     // }
 
     return data;
-
   } catch (error) {
     console.log(error);
   }
--- blueprints/dash/static/dash/js/test1-widget.js
+++ blueprints/dash/static/dash/js/test1-widget.js
@@ -1,57 +1,66 @@
-window.addEventListener('load', setup);
+window.addEventListener("load", setup);
 
 async function setup() {
-  const ctx = document.getElementById('myChart').getContext('2d');
+  const ctx = document.getElementById("myChart").getContext("2d");
   const World = await getData();
-  const myChart = new Chart(ctx, {
-    type: 'line',
+  myChart = new Chart(ctx, {
+    type: "line",
     data: {
       labels: World.dates,
       datasets: [
         {
-          label: 'Confirmed Cases',
+          label: "Confirmed Cases",
           data: World.cases_sum,
           fill: false,
-          borderColor: 'rgba(255, 99, 132, 1)',
-          backgroundColor: 'rgba(255, 99, 132, 0.5)',
-          borderWidth: 1
-        }
-      ]
+          borderColor: "rgba(255, 99, 132, 1)",
+          backgroundColor: "rgba(255, 99, 132, 0.5)",
+          borderWidth: 2,
+        },
+      ],
     },
     options: {
+      scales: {
+        yAxes: [{}],
+      },
       title: {
         display: true,
-        text: 'Confirmed Cases (World)'
+        text: "Confirmed Cases (World)",
       },
       responsive: true,
       maintainAspectRatio: false,
-    }
+    },
   });
 }
+
 //get data from csv
+//Data From : Johns Hopkins
 async function getData() {
-  const response = await fetch('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv');
+  const response = await fetch(
+    "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv"
+  );
   var data = await response.text();
 
   const dates = [];
   var cases_sum = [];
-  const first_row = data.split('\n')[0].split(',');
+  const first_row = data.split("\n")[0].split(",");
 
   for (var i = 4; i < first_row.length; ++i) {
     var date = first_row[i].substring(0, 4);
-    if (date[date.length - 1] == '/') { date = date.substring(0, 3); }
+    if (date[date.length - 1] == "/") {
+      date = date.substring(0, 3);
+    }
     dates.push(date);
   }
 
-  const second_row = data.split('\n').slice(1)[0].split(',');
+  const second_row = data.split("\n").slice(1)[0].split(",");
 
   for (var i = 4; i < second_row.length; ++i) {
     cases_sum.push(parseInt(second_row[i]));
   }
 
-  const rows_after_two = data.split('\n').slice(2);
+  const rows_after_two = data.split("\n").slice(2);
   for (var i = 0; i < rows_after_two.length; ++i) {
-    const cols = rows_after_two[i].split(',');
+    const cols = rows_after_two[i].split(",");
     for (var j = 4; j < cols.length; ++j) {
       cases_sum[j] += parseInt(cols[j]);
     }
@@ -60,13 +69,20 @@ async function getData() {
   return { dates, cases_sum };
 }
 
-// //Select map tab (implement later)
-// $(".graph-tab").click(function(){
-//     $(".graph-tab").css("background-color", "whitesmoke");
-//     $(this).css("background-color", "rgb(211, 211, 211)");
+//Select map tab
+$(".graph-tab").click(function () {
+  $(".graph-tab").css("background-color", "whitesmoke");
+  $(this).css("background-color", "rgb(211, 211, 211)");
 
-//     if($(this).text() == "Linear"){
-
-//     }else if($(this).text() == "Logarithmic"){
-//     }
-//  });
+  if ($(this).text() == "Linear") {
+    if (myChart.options.scales.yAxes[0] != "linear") {
+      myChart.options.scales.yAxes[0] = { type: "linear" };
+      myChart.update();
+    }
+  } else if ($(this).text() == "Logarithmic") {
+    if (myChart.options.scales.yAxes[0] != "logarithmic") {
+      myChart.options.scales.yAxes[0] = { type: "logarithmic" };
+      myChart.update();
+    }
+  }
+});
